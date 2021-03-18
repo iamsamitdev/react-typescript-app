88e102d34996cbe586199d1c33d51e31d38dd3f6
Samit Koyom <samitkoyom@gmail.com>
2021-03-18 23:06:08 +0700

    update fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
+import './styles/font.scss'
 import './styles/main.scss'
 import './styles/custom.scss'
 import Routes from './Routes'
